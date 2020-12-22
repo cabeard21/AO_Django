56ba2cb8e975e1223e8ba847bdfc5eee5301b2a1
DESKTOP-ENBUIB3\cabeard21 <cabeard21@gmail.com>
2020-12-21 18:10:35 -0600

    Models implemented. Fixtures imported successfully.

--- Equipment/admin.py
+++ Equipment/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+@admin.register(EquipmentSet)
+class EquipmentSetAdmin(admin.ModelAdmin):
+    filter_horizontal = ('items',)
+
+admin.site.register(ItemTier)
+admin.site.register(Character)
+admin.site.register(ItemTypeSpec)
+
+admin.site.register(Item)
+admin.site.register(ItemType)
--- Equipment/models.py
+++ Equipment/models.py
@@ -18,15 +18,39 @@ class ItemTier(models.Model):
         return f"{self.item} > {self.min_tier}"
 
 
+# Fixture
 class Item(models.Model):
-
-    class QualityEnum(models.IntegerChoices):
-        NORMAL = 1,
-        GOOD = 2,
-        OUTSTANDING = 3,
-        EXCELLENT = 4,
-        MASTERPIECE = 5,
 
     item_name = models.CharField(max_length=50)
     tier = models.IntegerField(default=4)
-    quality = models.IntegerField(default=QualityEnum.NORMAL, choices=QualityEnum.choices)
+    item_type = models.ForeignKey('ItemType', on_delete=models.DO_NOTHING, related_name='item_itemtype')
+
+    def __str__(self):
+        return self.item_name
+
+
+class Character(models.Model):
+
+    char_name = models.CharField(max_length=100, unique=True)
+    mastery = models.ManyToManyField('ItemTypeSpec')
+
+    def __str__(self):
+        return self.char_name
+
+
+class ItemTypeSpec(models.Model):
+
+    item_type = models.ForeignKey('ItemType', on_delete=models.DO_NOTHING, related_name='itemtypespec_itemtype')
+    spec_bonus = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.item_type}: {self.spec_bonus}"
+
+
+# Fixture
+class ItemType(models.Model):
+
+    item_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.item_type
